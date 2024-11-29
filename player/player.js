2a9e709abb7c98424558f2d0f53fb42a82ca04f1
rin2401 <rin2401@gmail.com>
2024-11-30 01:40:22 +0700

    Update player

--- player/player.js
+++ player/player.js
@@ -1,24 +1,24 @@
 var video = document.getElementById('video');
 
 function playM3u8(url){
-  if(Hls.isSupported()) {
-      video.volume = 0.3;
-      var hls = new Hls();
-      var m3u8Url = decodeURIComponent(url)
-      hls.loadSource(m3u8Url);
-      hls.attachMedia(video);
-      hls.on(Hls.Events.MANIFEST_PARSED,function() {
-        video.play();
-      });
-      document.title = url
+    if(Hls.isSupported()) {
+        video.volume = 0.3;
+        var hls = new Hls();
+        var m3u8Url = decodeURIComponent(url)
+        hls.loadSource(m3u8Url);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function() {
+            video.play();
+        });
+        // document.title = url
     }
 	else if (video.canPlayType('application/vnd.apple.mpegurl')) {
 		video.src = url;
-		video.addEventListener('canplay',function() {
-		  video.play();
+		video.addEventListener('canplay', function() {
+		    video.play();
 		});
 		video.volume = 0.3;
-		document.title = url;
+		// document.title = url;
   	}
 }
 
@@ -55,9 +55,10 @@ function vidFullscreen() {
 function seekTime() {
     if (window.location.href.split("#").length >= 3) {
         video.currentTime = Number(window.location.href.split("#")[2])
-        console.log("video.currentTime:", video.currentTime);
     }
 }
+
+console.log(window.location.href.split("#"))
 
 playM3u8(window.location.href.split("#")[1])
 seekTime()
