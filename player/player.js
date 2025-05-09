6ede43b34424676b46cf529954389f525d35d624
rin2401 <rin2401@gmail.com>
2025-05-09 13:44:05 +0700

    Add artwork 128

--- player/player.js
+++ player/player.js
@@ -108,12 +108,12 @@ function setMediaSession(title) {
         navigator.mediaSession.metadata = new MediaMetadata({
             title: title,
             artwork: [
-                // {
-                //     src: 'https://rin2401.github.io/anime/player/r3_128.png', sizes: '128x128',
-                //     type: 'image/png'
-                // },
                 {
                     src: 'https://rin2401.github.io/anime/player/r3_512.png', sizes: '512x512',
+                    type: 'image/png'
+                },
+                {
+                    src: 'https://rin2401.github.io/anime/player/r3_128.png', sizes: '128x128',
                     type: 'image/png'
                 },
             ]
