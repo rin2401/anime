6b5a37e59cf3db19dfb92f687cb8d1b853579484
rin2401 <rin2401@gmail.com>
2025-05-25 21:42:35 +0700

    Add fire playlist

--- scripts/fire.py
+++ scripts/fire.py
@@ -10,10 +10,23 @@ firebase_admin.initialize_app(cred, {
 })
 
 def update_ep(title, m3u8_data, fire_path):
-    users_ref = db.reference(fire_path)
-    users_ref.update({
+    ref = db.reference(fire_path)
+    ref.update({
         "title": title,
         'm3u8': m3u8_data
     })
 
     return f"{DB_URL}/{fire_path}.json"
+
+
+def update_anime(anime_id, data):
+    for d in data:
+        id = d["title"].split(" ")[1]
+        ref = db.reference(f"anime/{anime_id}/{id}")
+        ref.update(d)
+
+if __name__ == "__main__":
+    data = [
+        { "title": "Tập 160", "file": "https://s5.phim1280.tv/20250108/RMahnuWi/2000kb/hls/index.m3u8", "type": "hls" },
+    ]
+    update_anime("2471", data)
